b58de84433bf3358868b0732bfd92c3bd5483e76
eldar-elbrus <elbrus56@mail.ru>
2022-08-24 18:43:16 +0500

    Master branch - Second Commit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 a = 2
 b = 10
 
@@ -9,3 +8,9 @@ if count(a,b) <= 100:
     print("Hello world")
 else:
     print(count(a,b))
+
+my_dict = {'a':5,'b':6, 'c':{'d':7,'e':8}, 'f':[1,2,3,4]}
+
+for i in my_dict.items():
+    print(i)
+
