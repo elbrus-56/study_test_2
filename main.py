84bb2723e369380c0c9acb8fc65fbd5291b323d8
eldar-elbrus <elbrus56@mail.ru>
2022-08-24 18:59:40 +0500

    Future branch - For Commit

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ my_dict = {'a':5,'b':6, 'c':{'d':7,'e':8}, 'f':[1,2,3,4]}
 for i in my_dict.items():
     print(i)
 
+def rec(m):
+    while m > 0:
+        m -= 1
+        yield m
+n = iter(rec(19))
+
+print(next(n))
+print(next(n))
+print(next(n))
+print(next(n))
+
+
